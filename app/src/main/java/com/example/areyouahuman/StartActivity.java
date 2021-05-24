bc0d5245acc7b76e9ccb62efe6a46771c56495ef
alex.polyakov94 <alex.polyakov94@yandex.ru>
2021-05-24 11:54:36 +0300

    upd

--- app/src/main/java/com/example/areyouahuman/StartActivity.java
+++ app/src/main/java/com/example/areyouahuman/StartActivity.java
@@ -3,6 +3,7 @@ package com.example.areyouahuman;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,8 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("System info", "Метод onCreate() запущен!");
         setContentView(R.layout.activity_start);
         startBtn = findViewById(R.id.btnStart);
         startBtn.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +26,40 @@ public class StartActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d("System info", "Метод onStart() запущен!");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d("System info", "Метод onResume() запущен!");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d("System info", "Метод onSaveInstanceState() запущен!");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d("System info", "Метод onPause() запущен!");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d("System info", "Метод onStop() запущен!");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("System info", "Метод onDestroy() запущен!");
     }
 }
